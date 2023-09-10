41753be91f64765b8305cda7798824f7a2c469ff
bob <97455610+Shresth72@users.noreply.github.com>
2023-09-10 13:57:53 +0530

    optimization

--- my-app/src/app/api/comment/route.ts
+++ my-app/src/app/api/comment/route.ts
@@ -9,22 +9,25 @@ export const POST = async (req: NextRequest) => {
 
     const commentId = nanoid();
 
-    //add comment to list
-    await redis.rpush("comments", commentId);
-
-    //add tags to comments
-    await redis.sadd(`tags:${commentId}`, tags);
-
-    // //retrieve and store comment details
+    //retrieve and store comment details
     const comment = {
       text,
       timestamp: new Date(),
       author: req.cookies.get("userId")?.value,
     };
 
-    console.log(comment);
+    //Optimization
+    await Promise.all([
+      //add comment to list
+      redis.rpush("comments", commentId),
 
-    // await redis.hset(`comment_details:${commentId}`, comment);
+      //add tags to comments
+      redis.sadd(`tags:${commentId}`, tags),
+
+      redis.hset(`comment_details:${commentId}`, comment),
+    ]);
+
+    console.log(comment);
 
     return new Response("OK");
   } catch (error) {
